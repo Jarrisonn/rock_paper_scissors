632862c690f550eaec6c9ca1d740ce7cd3836112
Josh Harrison <joshharrison@hotmail.co.uk>
2021-07-06 18:27:20 +0100

    Added loop so game is played 5 times and logs in the console the scores and who has won and lost

--- script.js
+++ script.js
@@ -1,31 +1,73 @@
-let result;
+let cpuResult;
+let userSelect;
+let userScore = 0;
+let cpuScore = 0;
 computerPlay = () => {
     
     min = Math.ceil(1);
     max = Math.floor(3);
     computerNum = Math.floor(Math.random() * (max - min + 1) + min);
     if (computerNum == 3) {
-        result = "Scissors";
+        cpuResult = "scissors";
     } else if (computerNum == 2) {
-        result = "Paper"
+        cpuResult = "paper"
     } else if (computerNum == 1) {
-        result = "Rock"
+        cpuResult = "rock"
     }
-    return result;
+    return cpuResult;
 }
 
 
-userPlay = result => {
-    let userSelect = window.prompt("Select Rock, Paper or Scissors");
+userPlay = cpuResult => {
+    userSelect = window.prompt("Select Rock, Paper or Scissors");
     userSelect = userSelect.toLowerCase();
     console.log(`the user has selected ${userSelect}`);
-    console.log(`the computer has selected ${result}`);
+    console.log(`the computer has selected ${cpuResult}`);
+    return userSelect;
 }
 
-game = () => {
-    
-    
+game = (cpuResult, userSelect) => {
+  if (cpuResult == userSelect) {
+    console.log("Tie");
+  } else if (cpuResult == "rock" && userSelect == "paper") {
+    userScore++;
+    console.log("User wins, paper beats rock");
+  } else if (cpuResult == "paper" && userSelect == "rock") {
+    cpuScore++;
+    console.log("CPU wins, paper beats rock");
+  } else if (cpuResult == "paper" && userSelect == "scissors") {
+    userScore++;
+    console.log("User wins, scissors beats paper");
+  } else if (cpuResult == "scissors" && userSelect == "paper") {
+    cpuScore++;
+    console.log("CPU wins, scissors beats paper");
+  } else if (cpuResult == "scissors" && userSelect == "rock") {
+    userScore++;
+    console.log("User wins, rock beats scissors");
+  } else if (cpuResult == "rock" && userSelect == "scissors") {
+    cpuScore++;
+    console.log("CPU wins, rock beats scissors");
+  }
+  console.log(`User score is ${userScore}`);
+  console.log(`CPU score is ${cpuScore}`);
 
+};
+
+
+//play 5 games
+playGame = () =>{
+    for(let i = 0; i < 5; i++){
+        computerPlay();
+        userPlay(cpuResult);
+        game(cpuResult, userSelect);
+    }
+    if(userScore > cpuScore){
+        console.log(`The user wins scoring ${userScore} and the cpu loses scoring ${cpuScore}`);
+    }else if(cpuScore > userScore){
+        console.log(`The user loses scoring ${userScore} and the cpu wins scoring ${cpuScore}`);
+    }else{
+        console.log(`the game is a tie with cpu and user scoring ${userScore}`);
+    }
 }
-computerPlay();
-userPlay(result);
+
+playGame();
